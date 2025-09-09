8ab2b87379879b8a0f900ca3afd071e343bdf574
kpiekut <karol.piekut@pm.me>
2025-09-09 07:37:57 +0100

    add button selectors

--- script.js
+++ script.js
@@ -42,5 +42,64 @@ readButton.setAttribute('id', 'readButton');
 menuDivButtonsDiv.appendChild(readButton);
 readButton.innerText = 'read';
 
+//buttons query selectors 
+let queueButtonSelector = document.querySelector('#queueButton');
+let booksButtonSelector = document.querySelector('#booksButton');
+let authorsButtonSelector = document.querySelector('#authorsButton');
+let readButtonSelector = document.querySelector('#readButton');
+
+//bodyDiv
+let bodyTable = document.createElement('table');
+bodyTable.setAttribute('id', 'bodyTable');
+bodyDiv.appendChild(bodyTable);
+
+//table headers 
+
+
+
+
+let authorTable = `
+<tr>
+  <th id='authorId'>id</th>
+  <th id='authorName'>Name</th>
+  <th id='authorSurname'>Surname</th>
+</tr>
+`;
+
+
+let queueTable;
+
+//functions
+
+function generateAuthorsHeader() {
+  let authorHeader = document.createElement('tr');
+
+  let authorIdHeader = document.createElement('th');
+  authorIdHeader.setAttribute('id', 'authorName');
+  authorIdHeader.innerText = 'id';
+  authorHeader.appendChild(authorIdHeader);
+
+  let authorNameHeader = document.createElement('th');
+  authorNameHeader.setAttribute('id', 'authorName');
+  authorNameHeader.innerText = 'Name';
+  authorHeader.appendChild(authorNameHeader);
+
+  let authorSurnameHeader = document.createElement('th');
+  authorSurnameHeader.setAttribute('id', 'authorSurname');
+  authorSurnameHeader.innerText = 'Surname';
+  authorHeader.appendChild(authorSurnameHeader);
+
+  return authorHeader;
+}
+
+
+function displayTableContent() {
+  let selectTable = document.querySelector('#bodyTable');
+  selectTable.appendChild(generateAuthorsHeader());
+}
+
+
+displayTableContent();
+
 
 
